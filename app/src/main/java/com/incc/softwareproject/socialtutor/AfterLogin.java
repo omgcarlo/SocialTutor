5da6224dd9fb773a2472de298746a1d7faf4b761
omgcarlo <carlojacalan1@gmail.com>
2015-12-14 18:50:46 -0800

    -events calendar +explore

--- app/src/main/java/com/incc/softwareproject/socialtutor/AfterLogin.java
+++ app/src/main/java/com/incc/softwareproject/socialtutor/AfterLogin.java
@@ -1,7 +1,7 @@
 package com.incc.softwareproject.socialtutor;
 
 
-//import android.annotation.TargetApi;
+
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,7 +15,7 @@ import android.widget.CalendarView;
 import android.widget.Toast;
 import android.widget.TextView;
 import android.graphics.Typeface;
-//import android.widget.CalendarView;
+
 
 /**
  * Created by carlo on 07/12/2015.
@@ -39,7 +39,7 @@ public class AfterLogin extends ActionBarActivity {
 
         //change font bai
         TextView toolbarTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
-        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "Admiration_Pains.ttf");
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "VintageOne.ttf");
         toolbarTitle.setTypeface(myTypeface);
 
         // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
@@ -63,47 +63,9 @@ public class AfterLogin extends ActionBarActivity {
 
         // Setting the ViewPager For the SlidingTabsLayout
         tabs.setViewPager(pager);
-        //initializeCalendar();
 
     }
-    /*@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public void initializeCalendar() {
 
-        calendar = (CalendarView) findViewById(R.id.calendar);
-
-        // sets whether to show the week number.
-        calendar.setShowWeekNumber(false);
-        // sets the first day of week according to Calendar.
-
-        // here we set Monday as the first day of the Calendar
-        calendar.setFirstDayOfWeek(2);
-        //The background color for the selected week.
-        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.green));
-
-        //sets the color for the dates of an unfocused month.
-
-        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
-
-        //sets the color for the separator line between weeks.
-
-        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.transparent));
-
-        //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
-
-        calendar.setSelectedDateVerticalBar(R.color.darkgreen);
-
-        //sets the listener to be notified upon selected date change.
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            //show the selected date as a toast
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
-                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
-
-            }
-
-        });
-
-    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
